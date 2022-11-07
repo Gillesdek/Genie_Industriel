db47afe6560a8c4fed82a9a290ec443f1e407a1a
Gilles Deknache <gilou.2207@gmail.com>
2022-11-07 09:22:35 +0100

    modification

--- main.py
+++ main.py
@@ -11,23 +11,23 @@ if __name__ == "__main__":
       
 
    
-    #print("1. Petit atelier: -------")
+    print("1. Petit atelier: -------")
    
-    #print("a. Nombre d'itération : ------")
+    print("a. Nombre d'itération : ------")
     
-    #MaxPopul = 50
-    #NbSelect = 30
-    #Nbiter = 150
-    #Pmut = 0.3
-    #Pcrois = 1
-    #Popul_base = Générer(MaxPopul,n1)
+    MaxPopul = 50
+    NbSelect = 30
+    Nbiter = 150
+    Pmut = 0.3
+    Pcrois = 1
+    Popul_base = Générer(MaxPopul,n1)
     
-    #Popul,cout,L = genetique(Popul_base,MaxPopul,n1,NbSelect,Pcrois,Pmut,Nbiter,D1,OF1,V1,afficher=True)
+    Popul,cout,L = genetique(Popul_base,MaxPopul,n1,NbSelect,Pcrois,Pmut,Nbiter,D1,OF1,V1,afficher=True)
     
-    #X = list(range(Nbiter))
+    X = list(range(Nbiter))
     
-    #plt.plot(X,L)
-    #plt.show()
+    plt.plot(X,L)
+    plt.show()
     
     #print("b. pourcentage d'obtention de la solution optimal: ------")
     #reussi = 0
@@ -45,25 +45,25 @@ if __name__ == "__main__":
     #print("pourcentage de réussite : " + "---------" + str((reussi/50)*100) + "% " + "-----------")
     #print("Moyenne Générale : " + "----------------" + str(S/50) + "-------------")
     
-    print("c. Influence de Pmut ----------------")
+    #print("c. Influence de Pmut ----------------")
     # Paramètre fixe :
     
-    MaxPopul = 50
-    NbSelect = 30
-    Nbiter = 150
-    Pcrois = 1
-    Popul_base = Générer(MaxPopul,n1)
+    #MaxPopul = 50
+    #NbSelect = 30
+    #Nbiter = 150
+    #Pcrois = 1
+    #Popul_base = Générer(MaxPopul,n1)
     
-    X = np.linspace(0,0.5,50)
-    Y = []
-    for x in X :
-        Popul,cout,L = genetique(Popul_base,MaxPopul,n1,NbSelect,Pcrois,x,Nbiter,D1,OF1,V1)
-        Y.append(cout[0])
+    #X = np.linspace(0,0.5,50)
+    #Y = []
+    #for x in X :
+        #Popul,cout,L = genetique(Popul_base,MaxPopul,n1,NbSelect,Pcrois,x,Nbiter,D1,OF1,V1)
+        #Y.append(cout[0])
     
-    plt.plot(X,Y)
-    plt.show()    
+    #plt.plot(X,Y)
+    #plt.show()    
         
-    print("conclusion : on choisi pmut entre 0.2 et 0.4 pour de meilleur résultat")    
+    #print("conclusion : on choisi pmut entre 0.2 et 0.4 pour de meilleur résultat")    
     
     
     #print("2. Grand atelier: -------")
